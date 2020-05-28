955e62879aa62682d78b56a9de0ee41ed0176282
Vitor Caetano <vitorlc95@gmail.com>
2020-05-28 17:50:21 -0300

    Remove department

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -100,7 +101,7 @@ public class SellerDaoJDBC implements SellerDao {
 			
 			st.executeUpdate();
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
 		}
